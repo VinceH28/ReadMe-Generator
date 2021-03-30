829cf8d8d05a55563241aac34f1195509e6a4351
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-03-30 16:22:51 -0400

    Update index.js

--- Utilis/index.js
+++ Utilis/index.js
@@ -5,3 +5,24 @@ const fs = require('fs');
 const util = require('util');
 const asyncWrite = util.promisify(fs.writeFile);
 
+//Funtion to run app (info = data from prompts)
+const generateMD = (info) => {
+    let licenseChoice = '';
+switch (info.license){
+    case "IBM":
+        licenseChoice = '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)'
+    break;
+    case "MIT":
+        licenseChoice = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+    break;
+    case "Mozilla":
+        licenseChoice = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
+    break;
+    case "Open Data Commons":
+        licenseChoice = '[![License: Open Data Commons Attribution](https://img.shields.io/badge/License-ODC_BY-brightgreen.svg)](https://opendatacommons.org/licenses/by/)'
+    break;
+    case "None":
+        licenseChoice = " "
+    break;
+}
+
